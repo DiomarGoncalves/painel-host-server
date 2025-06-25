e6df6cdd2a87fdb72cec36604038358e75489c1f
DiomarGoncalves <50972479+DiomarGoncalves@users.noreply.github.com>
2025-06-25 08:58:28 -0300

    update

---
+++ server-files/development_behavior_packs/Edds' VeinCapitator v2/subpacks/treecapitator/scripts/main.js
@@ -0,0 +1,16 @@
+import { treecapitatorManager } from './classes/treecapitatorManager';
+import { veinminerManager } from './classes/veinminerManager';
+import { world, EquipmentSlot, EntityEquippableComponent } from '@minecraft/server';
+world.beforeEvents.playerBreakBlock.subscribe((data) => {
+    const { block, dimension, itemStack, player } = data;
+    if (!itemStack) return;
+    const mainhand = player.getComponent(EntityEquippableComponent.componentId).getEquipmentSlot(EquipmentSlot.Mainhand);
+    const item = mainhand.getItem();
+    if (!item) return;
+    if (!player.isSneaking) return;
+    if (item.hasTag("minecraft:is_axe")) {
+        if (treecapitatorManager.logIDs.includes(block.typeId)) {
+            treecapitatorManager.start(player, block, item, mainhand);
+        }
+    }
+});
